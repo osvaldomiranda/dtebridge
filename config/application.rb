bc17187a8ae5d99b7ebada34a73d6cb652c16cc2
Osvaldo Miranda S <osvaldo.omiranda@gmail.com>
2014-10-09 13:44:08 -0300

    [ping]

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Dtebridge
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w( *.svg *.eot *.woff *.ttf *.png *.jpg)
 
     config.generators do |g|
       g.template_engine :haml
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 set :ip, "200.72.145.75"
 server "#{ip}", :web, :app, :db, primary: true
 set :rails_env, 'production'
+set :branch, "master"
