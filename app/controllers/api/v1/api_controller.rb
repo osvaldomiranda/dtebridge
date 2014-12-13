9a2f2b32ac0bd52f0cc54de7d4e038b85f6f9a99
Osvaldo Miranda S <osvaldo.omiranda@gmail.com>
2014-12-13 09:41:08 -0300

    [ADD Column] estadoxml

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ApiController < ApplicationController
-  skip_before_action :require_login
+#  skip_before_action :require_login
   
   protect_from_forgery with: :null_session
   # Includes helpers in controller
