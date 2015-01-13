6ff1dd704a667dfcc6a40838c5c603d57e52ebb4
Osvaldo Miranda S <osvaldo.omiranda@gmail.com>
2015-01-13 14:49:40 -0300

    Libro Venta

--- app/controllers/libro_venta_controller.rb
+++ app/controllers/libro_venta_controller.rb
@@ -1,6 +1,6 @@
 class LibroVentaController < ApplicationController
   def index
-    @documentos = Documento.select('"TipoDTE", sum("MntNeto") as MntNeto,sum("MntExe") as MntExe, sum("IVA") as IVA, sum("MntTotal") as MntTotal ').group('"TipoDTE"') 
+    @documentos = Documento.select('"TipoDTE", sum("MntNeto") as MntNeto,sum("MntExe") as MntExe, sum("IVA") as IVA, sum("MntTotal") as MntTotal ').group(:id,'"TipoDTE"') 
     @otrosImps = ImpuestoReten.select('"TipoImp", "TasaImp", sum("MontoImp") as MontoImp').group('"TipoImp","TasaImp"')
     puts "****************"
     puts @documentos.first
