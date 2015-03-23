28691078fb4fdc5a17227dc7a4f73e52b5f2ee33
Osvaldo Miranda S <osvaldo.omiranda@gmail.com>
2015-03-22 22:53:36 -0300

    [FIX] ping

--- app/controllers/api/v1/iat_controller.rb
+++ app/controllers/api/v1/iat_controller.rb
@@ -21,32 +21,34 @@ class Api::V1::IatController < Api::V1::ApiController
   end
 
   def ping
-    paramJson = eval(params[:udte].force_encoding('iso-8859-1').encode('utf-8'))
+    unless params[:udte].nil?
+      paramJson = eval(params[:udte].force_encoding('iso-8859-1').encode('utf-8'))
 
-    sucursal = paramJson["CdgSIISucur"].to_s || "0"
-    rut = paramJson["RutEmisor"] || "1"
-    tipoDte = paramJson["TipoDTE"] || 0
-    folio = paramJson["Folio"] || 0
+      sucursal = paramJson["CdgSIISucur"].to_s || "0"
+      rut = paramJson["RutEmisor"] || "1"
+      tipoDte = paramJson["TipoDTE"] || 0
+      folio = paramJson["Folio"] || 0
 
-    log = LogIat.where(rut: rut).where(sucursal: sucursal).first
+      log = LogIat.where(rut: rut).where(sucursal: sucursal).first
 
-    if !log.nil?
-      log.ultimo_ping = Time.now
-      log.ultimoping = Time.now
-      log.tipo_dte = tipoDte
-      log.ultimo_folio = folio
-      log.udtejson = paramJson.to_s
-      log.save
-    else  
-      log = LogIat.new
-      log.sucursal = sucursal
-      log.rut = rut
-      log.ultimo_ping = Time.now
-      log.ultimoping = Time.now
-      log.tipo_dte = tipoDte
-      log.ultimo_folio = folio
-      log.udtejson = paramJson.to_s
-      log.save
+      if !log.nil?
+        log.ultimo_ping = Time.now
+        log.ultimoping = Time.now
+        log.tipo_dte = tipoDte
+        log.ultimo_folio = folio
+        log.udtejson = paramJson.to_s
+        log.save
+      else  
+        log = LogIat.new
+        log.sucursal = sucursal
+        log.rut = rut
+        log.ultimo_ping = Time.now
+        log.ultimoping = Time.now
+        log.tipo_dte = tipoDte
+        log.ultimo_folio = folio
+        log.udtejson = paramJson.to_s
+        log.save
+      end
     end
     render "/api/v1/iat/ping"
   end
