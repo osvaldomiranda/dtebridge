8bf85d1f1a0d5558fc2815e9d5b8f8ca5ca5d9a9
Osvaldo Miranda S <osvaldo.omiranda@gmail.com>
2015-06-27 15:56:25 -0300

    [ADD] fix compmanual

--- app/models/compmanual.rb
+++ app/models/compmanual.rb
@@ -12,67 +12,71 @@ class Compmanual < ActiveRecord::Base
     # header = spreadsheet.row(1)
     # (2..spreadsheet.last_row).each do |i|
 
+    fileOk = true
     CSV.foreach(file.path, col_sep: ';', headers: true) do |row|
+      # rowHash = row.to_hash 
+    end
 
-    rowHash = row.to_hash    
-    # if ! rowHash.valid_encoding?
-    #   rowHash = row.encode("UTF-16be", :invalid=>:replace, :replace=>"?").encode('UTF-8')
-    # end
+    if fileOk
+      CSV.foreach(file.path, col_sep: ';', headers: true) do |row|
 
-      # rowHash = Hash[[header, spreadsheet.row(i)].transpose]
+      rowHash = row.to_hash    
 
-      doc = Compmanual.new(rowHash)
 
-      if doc.rutrecep.index('-').nil?  
-        doc.rutrecep = doc.rutrecep.insert(8, '-') 
-      end  
-      if doc.tipodoc == 1
-        doc.tipodoc = 30
+        doc = Compmanual.new(rowHash)
+
+        if doc.rutrecep.index('-').nil?  
+          doc.rutrecep = doc.rutrecep.insert(8, '-') 
+        end  
+        if doc.tipodoc == 1
+          doc.tipodoc = 30
+        end
+        if doc.tipodoc == 7
+          doc.tipodoc = 35
+        end  
+        doc.save
+
+        impto10 = rowHash["impto10"].to_f
+        impto18 = rowHash["impto18"].to_f
+        impto25 = rowHash["impto25"].to_f
+        impto30 = rowHash["impto30"].to_f
+
+        if impto10>0
+          imptoH = Hash.new 
+          imptoH["TipoImp"] = 10   
+          imptoH["TasaImp"] = 10 
+          imptoH["MontoImp"] = impto10 
+          imptoH["compmanual_id"] = doc.id
+          Otrosimpcompmanual.create! imptoH
+        end  
+        if impto18>0
+          imptoH = Hash.new 
+          imptoH["TipoImp"] = 18   
+          imptoH["TasaImp"] = 18 
+          imptoH["MontoImp"] =impto18 
+          imptoH["compmanual_id"] =doc.id
+          Otrosimpcompmanual.create! imptoH
+        end  
+        if impto25>0
+          imptoH = Hash.new 
+          imptoH["TipoImp"] = 25   
+          imptoH["TasaImp"] = 25 
+          imptoH["MontoImp"] = impto25 
+          imptoH["compmanual_id"] = doc.id
+          Otrosimpcompmanual.create! imptoH
+        end  
+        if impto30>0
+          imptoH = Hash.new 
+          imptoH["TipoImp"] = 30   
+          imptoH["TasaImp"] = 30 
+          imptoH["MontoImp"] = impto30 
+          imptoH["compmanual_id"] = doc.id
+          Otrosimpcompmanual.create! imptoH
+        end  
+        doc.otrosimpto = impto10 + impto18 + impto25 + impto30
+        doc.save
+
       end
-      if doc.tipodoc == 7
-        doc.tipodoc = 35
-      end  
-      doc.save
-
-      impto10 = rowHash["impto10"].to_f
-      impto18 = rowHash["impto18"].to_f
-      impto25 = rowHash["impto25"].to_f
-      impto30 = rowHash["impto30"].to_f
-
-      if impto10>0
-        imptoH = Hash.new 
-        imptoH["TipoImp"] = 10   
-        imptoH["TasaImp"] = 10 
-        imptoH["MontoImp"] = impto10 
-        imptoH["compmanual_id"] = doc.id
-        Otrosimpcompmanual.create! imptoH
-      end  
-      if impto18>0
-        imptoH = Hash.new 
-        imptoH["TipoImp"] = 18   
-        imptoH["TasaImp"] = 18 
-        imptoH["MontoImp"] =impto18 
-        imptoH["compmanual_id"] =doc.id
-        Otrosimpcompmanual.create! imptoH
-      end  
-      if impto25>0
-        imptoH = Hash.new 
-        imptoH["TipoImp"] = 25   
-        imptoH["TasaImp"] = 25 
-        imptoH["MontoImp"] = impto25 
-        imptoH["compmanual_id"] = doc.id
-        Otrosimpcompmanual.create! imptoH
-      end  
-      if impto30>0
-        imptoH = Hash.new 
-        imptoH["TipoImp"] = 30   
-        imptoH["TasaImp"] = 30 
-        imptoH["MontoImp"] = impto30 
-        imptoH["compmanual_id"] = doc.id
-        Otrosimpcompmanual.create! imptoH
-      end  
-      doc.otrosimpto = impto10 + impto18 + impto25 + impto30
-      doc.save
     end
   end
 
