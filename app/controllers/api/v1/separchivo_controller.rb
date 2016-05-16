10628ed684271460bfc4d7cb62062811390b716e
osvaldomiranda <osvaldo.omiranda@gmail.com>
2016-05-16 13:25:04 -0400

    [ADD] separchivo

--- app/controllers/api/v1/separchivo_controller.rb
+++ app/controllers/api/v1/separchivo_controller.rb
@@ -1,15 +1,19 @@
 class Api::V1::SeparchivoController < Api::V1::ApiController
   def separchivo
-    documentos = Documento.all
+   
     empresas = Documento.uniq.pluck(:RUTEmisor)
 
     empresas.each do |rut|
       system("mkdir #{rut}")
+
+      documentos = Documento.where("RUTEmisor= ?", rut)
+      
+        documentos.each do |documento|
+        system("mv #{documento.pdfs.url}  /#{documento.RUTEmisor}#{documento.pdfs_identifier}") 
+      end
     end
 
-    documentos.each do |documento|
-      system("mv #{documento.pdfs.url}  /#{documento.RUTEmisor}#{documento.pdfs_url}") 
-    end
+
 
     render json: "Ok"
   end
